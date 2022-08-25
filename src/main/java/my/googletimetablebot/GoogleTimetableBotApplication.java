0c6c15e88d636d567f7482957cbd6d8d15a90134
Gleb Padergin <glebpad@yandex.ru>
2022-08-25 20:13:24 +0300

    init commit

---
+++ src/main/java/my/googletimetablebot/GoogleTimetableBotApplication.java
@@ -0,0 +1,13 @@
+package my.googletimetablebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleTimetableBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleTimetableBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/googletimetablebot/GoogleTimetableBotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.googletimetablebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleTimetableBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
